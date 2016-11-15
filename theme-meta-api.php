0f3d68903a0c53467bcbcb2c765c7fe9207f28e1
miya0001 <miya@wpist.me>
2016-11-16 04:12:55 +0900

    add theme meta api

--- theme-meta-api.php
+++ theme-meta-api.php
@@ -10,12 +10,14 @@ register_activation_hook( __FILE__, 'my_activation_callback' );
 function my_activation_callback() {
 	add_rewrite_endpoint( 'theme-features', EP_ROOT );
 	add_rewrite_endpoint( 'theme-tags', EP_ROOT );
+	add_rewrite_endpoint( 'theme-meta', EP_ROOT );
 	flush_rewrite_rules();
 }
 
 add_action( "init", function() {
 	add_rewrite_endpoint( 'theme-features', EP_ROOT );
 	add_rewrite_endpoint( 'theme-tags', EP_ROOT );
+	add_rewrite_endpoint( 'theme-meta', EP_ROOT );
 } );
 
 add_action( 'template_redirect', function() {
@@ -29,6 +31,17 @@ add_action( 'template_redirect', function() {
 		$theme = wp_get_theme();
 		header( "content-type: application/json" );
 		echo json_encode( $theme->get( "Tags" ) );
+		exit;
+	} elseif ( isset( $wp_query->query['theme-meta'] ) ) {
+		$theme = wp_get_theme();
+		header( "content-type: application/json" );
+		echo json_encode( array(
+			"name" => $theme->name,
+			"version" => $theme->version,
+			"stylesheet" => $theme->stylesheet,
+			"template" => $theme->template,
+			"textdomain" => $theme->get( "TextDomain" ),
+		) );
 		exit;
 	}
 } );
