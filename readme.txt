1718a2f3f3b018a78c3f3172b33197c953cf6ea2
miya0001 <miya@wpist.me>
2016-11-16 00:27:49 +0900

    first commit

---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== Theme Meta API ===
+Contributors: miyauchi
+Tags: theme, review
+Requires at least: 4.6
+Tested up to: 4.6.1
+Stable tag: 0.1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Displays the metadata of the current theme as JSON API.
+
---
+++ theme-meta-api.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name:     Theme Meta API
+ * Plugin URI:      https://github.com/miya0001/theme-meta-api
+ * Version:         0.1.0
+ */
+
+register_activation_hook( __FILE__, 'my_activation_callback' );
+
+function my_activation_callback() {
+	add_rewrite_endpoint( 'theme-features', EP_ROOT );
+	add_rewrite_endpoint( 'theme-tags', EP_ROOT );
+	flush_rewrite_rules();
+}
+
+add_action( "init", function() {
+	add_rewrite_endpoint( 'theme-features', EP_ROOT );
+	add_rewrite_endpoint( 'theme-tags', EP_ROOT );
+} );
+
+add_action( 'template_redirect', function() {
+	global $wp_query;
+	if ( isset( $wp_query->query['theme-features'] ) ) {
+		global $_wp_theme_features;
+		header( "content-type: application/json" );
+		echo json_encode( $_wp_theme_features );
+		exit;
+	} elseif ( isset( $wp_query->query['theme-tags'] ) ) {
+		$theme = wp_get_theme();
+		header( "content-type: application/json" );
+		echo json_encode( $theme->get( "Tags" ) );
+		exit;
+	}
+} );
